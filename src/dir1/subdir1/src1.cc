c10951bbe2a2630f4417ff4019b085b5a9a20b19
Ryan Burn <ryan.burn@gmail.com>
2021-06-03 14:37:39 -0700

    setup reproduce

--- src/dir1/subdir1/src1.cc
+++ src/dir1/subdir1/src1.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-#include "src1.h"
+#include "dir1/subdir1/src1.h"
 
 int main() {
   std::cout << "sizeof(string) = " << SizeOf<std::string>::value << '\n';
